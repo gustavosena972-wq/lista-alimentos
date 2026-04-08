08c44810a76bd9dcf7f5e3c7a0719b1bb5b81226
gustavosena972-wq <gustavosena972@gmail.com>
2026-04-07 23:04:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function adicionar() {
+    const input = document.getElementById("inputAlimento");
+    const lista = document.getElementById("lista");
+
+    if (input.value.trim() === "") return;
+
+    const li = document.createElement("li");
+    li.textContent = input.value;
+
+    const botao = document.createElement("button");
+    botao.textContent = "X";
+    botao.className = "remover";
+
+    botao.onclick = function () {
+        lista.removeChild(li);
+    };
+
+    li.appendChild(botao);
+    lista.appendChild(li);
+
+    input.value = "";
+}
